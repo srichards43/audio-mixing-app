258377bf2ca6781e613a4b00e2e4a8bfaf346028
Scott Richards <sr845@kent.ac.uk>
2025-08-05 22:34:24 +0100

    Settings page, dark and light themes

--- app/src/main/java/com/example/audiomixer/MainActivity.java
+++ app/src/main/java/com/example/audiomixer/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.audiomixer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -10,13 +12,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class MainActivity extends AppCompatActivity {
 
+    /* temp
     SeekBar songVolBar;
     TextView songVolLabel;
     SeekBar ambienceVolBar;
 
-    // Add event listeners
     SeekBar.OnSeekBarChangeListener volumeListener = new SeekBar.OnSeekBarChangeListener() {
         @Override
         public void onProgressChanged(SeekBar seekBar, int progress, boolean bool) {
@@ -38,20 +42,32 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
+    */
+
+
+    public void launchSettings(View v) {
+        Intent i = new Intent(this, SettingsActivity.class);
+        startActivity(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("Home");
 
         // Find ids
+        /* temp
         songVolBar = findViewById(R.id.songVolumeBar);
         songVolLabel = findViewById(R.id.songVolumeLabel);
         ambienceVolBar = findViewById(R.id.ambienceVolumeBar);
 
         // Add listeners
         songVolBar.setOnSeekBarChangeListener(volumeListener);
+        */
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
